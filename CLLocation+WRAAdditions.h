d8849ef60cbccc7d245619f2d96f82ecf0a130b0
Logan Holmes <snown27@gmail.com>
2012-09-06 08:11:29 -0700

    Changed some heuristics about when to record location (including change in location and accuracy), and when to switch to Geofence location updates.

---
+++ CLLocation+WRAAdditions.h
@@ -0,0 +1,13 @@
+//
+//  CLLocation+WRAAdditions.h
+//  Meander
+//
+//  Created by Logan Holmes on 9/6/12.
+//  Copyright (c) 2012 WellRedApps. All rights reserved.
+//
+
+#import <CoreLocation/CoreLocation.h>
+
+@interface CLLocation (WRAAdditions)
+
+@end
---
+++ CLLocation+WRAAdditions.m
@@ -0,0 +1,13 @@
+//
+//  CLLocation+WRAAdditions.m
+//  Meander
+//
+//  Created by Logan Holmes on 9/6/12.
+//  Copyright (c) 2012 WellRedApps. All rights reserved.
+//
+
+#import "CLLocation+WRAAdditions.h"
+
+@implementation CLLocation (WRAAdditions)
+
+@end
