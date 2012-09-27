d2c41db5a4ec200243ee52cd358d79c9754c165d
Logan Holmes <snown27@gmail.com>
2012-09-27 11:41:25 -0700

    Adding Base Master Header

---
+++ WRAAdditions.h
@@ -0,0 +1,14 @@
+//
+//  WRAAdditions.h
+//  WRAAdditions
+//
+//  Created by Logan Holmes on 9/27/12.
+//  Copyright (c) 2012 WellRedApps. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+#import "NSArray+WRAAdditions.h"
+#import "NSCalendar+WRAAdditions.h"
+#import "NSMutableString+WRAAdditions.h"
+#import "NSString+WRAAdditions.h"
