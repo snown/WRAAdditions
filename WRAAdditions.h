e35c1963e265536559992d1529ce2bbcd61db9e6
Logan Holmes <snown27@gmail.com>
2012-09-28 14:26:54 -0700

    Added MAC OS and iOS Compiler directives
    NSImage is on Mac OS, but not iOS.

--- WRAAdditions.h
+++ WRAAdditions.h
@@ -6,10 +6,21 @@
 //  Copyright (c) 2012 WellRedApps. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
+#import <Foundation/Foundation.h>
 
 #import "NSArray+WRAAdditions.h"
 #import "NSCalendar+WRAAdditions.h"
 #import "NSMutableString+WRAAdditions.h"
 #import "NSString+WRAAdditions.h"
+
+#if TARGET_OS_MAC
+
 #import "NSImage+WRAAdditions.h"
+
+#endif
+
+#if TARGET_OS_IPHONE
+
+
+
+#endif
